dd05d0dabc6cf97f24cd0b41051aaceca9537ab0
Cypooos <60020572+Cypooos@users.noreply.github.com>
2026-03-30 13:14:18 +0200

    typst: Initial commit

---
+++ typst/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Typst";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.typst;
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.typst
+            pkgs.typst-lsp
+          ];
+        };
+      });
+}
